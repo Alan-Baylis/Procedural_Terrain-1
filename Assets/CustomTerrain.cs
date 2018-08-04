4ca6b55219877be83f8e90d6da70f7c7b4ae26fc
DESKTOP-6G328RK\fozzy <fozzyis@gmail.com>
2018-08-04 10:51:32 +1000

    Initial Generator complete

---
+++ Assets/CustomTerrain.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System;
+
+[ExecuteInEditMode]
+
+public class CustomTerrain : MonoBehaviour {
+
+    public Vector2 randomHeightRange = new Vector2(0, 0.1f);
+    public Texture2D heightMapImage;
+    public Vector3 heightMapScale = new Vector3(1, 1, 1);
+
+    public float perlinNoiseXScale = 0.01f;
+    public float perlinNoiseYScale = 0.01f;
+    public int perlinOffsetX = 0;
+    public int perlinOffsetY = 0;
+
+
+    public Terrain terrain;
+    public TerrainData terrainData;
+
+    public void LoadHeightMapImage()
+    {
+        float[,] heightMap = new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
+
+        for (int x = 0; x < terrainData.heightmapWidth; x++)
+        {
+            for (int z = 0; z < terrainData.heightmapHeight; z++)
+            {
+                heightMap[x, z] = heightMapImage.GetPixel((int)(x * heightMapScale.x), (int)(z * heightMapScale.z)).grayscale * heightMapScale.y;
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    public void RandomTerrain()
+    {
+        float[,] heightMap = terrainData.GetHeights(0,0,terrainData.heightmapWidth, terrainData.heightmapHeight);
+
+        for(int x = 0; x < terrainData.heightmapWidth; x++)
+        {
+            for(int y = 0; y < terrainData.heightmapHeight; y++)
+            {
+                heightMap[x, y] += UnityEngine.Random.Range(randomHeightRange.x, randomHeightRange.y);
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+
+    }
+
+    public void PerlinNoise()
+    {
+        float[,] heightMap = terrainData.GetHeights(0,0,terrainData.heightmapWidth, terrainData.heightmapHeight);
+
+        for (int x = 0; x < terrainData.heightmapWidth; x++)
+        {
+            for (int y = 0; y < terrainData.heightmapHeight; y++)
+            {
+                heightMap[x, y] = Mathf.PerlinNoise((x + perlinOffsetX) * perlinNoiseXScale, (y + perlinOffsetY) * perlinNoiseYScale);
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    public void ResetTerrain()
+    {
+        float[,] heightMap = new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
+
+        for (int x = 0; x < terrainData.heightmapWidth; x++)
+        {
+            for (int y = 0; y < terrainData.heightmapHeight; y++)
+            {
+                heightMap[x, y] = randomHeightRange.x;
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    private void OnEnable()
+    {
+        terrain = GetComponent<Terrain>();
+        terrainData = terrain.terrainData;
+    }
+
+    private void Awake()
+    {
+        SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+        SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+        AddTag(tagsProp, "Terrain");
+        AddTag(tagsProp, "Cloud");
+        AddTag(tagsProp, "Shore");
+
+        tagManager.ApplyModifiedProperties();
+
+        this.gameObject.tag = "Terrain";
+        
+    }
+
+    void AddTag(SerializedProperty tagsProp, string newTag)
+    {
+        bool found = false;
+
+        for(int i = 0; i < tagsProp.arraySize; i++)
+        {
+            SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+            if (t.stringValue.Equals(newTag)) { found = true; break; }
+        }
+
+        if (!found)
+        {
+            tagsProp.InsertArrayElementAtIndex(0);
+            SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+            newTagProp.stringValue = newTag;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using EditorGUITable;
+
+[CustomEditor(typeof(CustomTerrain))]
+[CanEditMultipleObjects]
+public class CustomTerrainEditor : Editor {
+
+    SerializedProperty randomHeightRange;
+    SerializedProperty heightMapScale;
+    SerializedProperty heightMapImage;
+    SerializedProperty perlinNoiseXScale;
+    SerializedProperty PerlinNoiseYScale;
+    SerializedProperty perlinOffsetX;
+    SerializedProperty PerlinOffsetY;
+    
+    bool showRandom = false;
+    bool showLoadHeights = false;
+    bool showPerlin = false;
+
+    private void OnEnable()
+    {
+        randomHeightRange = serializedObject.FindProperty("randomHeightRange");
+        heightMapScale = serializedObject.FindProperty("heightMapScale");
+        heightMapImage = serializedObject.FindProperty("heightMapImage");
+        perlinNoiseXScale = serializedObject.FindProperty("perlinNoiseXScale");
+        PerlinNoiseYScale = serializedObject.FindProperty("perlinNoiseYScale");
+        perlinOffsetX = serializedObject.FindProperty("perlinOffsetX");
+        PerlinOffsetY = serializedObject.FindProperty("perlinOffsetY");
+        
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        CustomTerrain terrain = (CustomTerrain)target;
+
+        showRandom = EditorGUILayout.Foldout(showRandom, "Random");
+        
+        if (showRandom)
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Set heights randomly between values", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(randomHeightRange);
+            if(GUILayout.Button("Random Heights"))
+            {
+                terrain.RandomTerrain();
+            }
+
+        }
+        showLoadHeights = EditorGUILayout.Foldout(showLoadHeights, "Height Map Texture");
+        
+        if (showLoadHeights)
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Load Height Map Texture", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(heightMapImage);
+            EditorGUILayout.PropertyField(heightMapScale);
+            if(GUILayout.Button("Load Texture"))
+            {
+                terrain.LoadHeightMapImage();
+            }
+        }
+
+        showPerlin = EditorGUILayout.Foldout(showPerlin, "Perlin Noise Generator");
+
+        if (showPerlin)
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Perlin Generator", EditorStyles.boldLabel);
+            EditorGUILayout.Slider(perlinNoiseXScale, 0, 0.05f, new GUIContent("X Scale"));
+            EditorGUILayout.Slider(PerlinNoiseYScale, 0, 0.05f, new GUIContent("Y Scale"));
+            EditorGUILayout.IntSlider(perlinOffsetX, 0, 10000, new GUIContent("X Offset"));
+            EditorGUILayout.IntSlider(PerlinOffsetY, 0, 10000, new GUIContent("Y Offset"));
+            if (GUILayout.Button("Run Generator"))
+            {
+                terrain.PerlinNoise();
+            }
+        }
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        if (GUILayout.Button("Reset Terrain"))
+        {
+            terrain.ResetTerrain();
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
